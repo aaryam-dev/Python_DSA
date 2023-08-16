dad60dcddb505b59be20826b5681d86bd80a56e5
aaryam-dev <73819294+aaryam-dev@users.noreply.github.com>
2023-08-16 10:53:12 +0530

    Create Min_Max_Heap.py

---
+++ Data Structures/7_Heap/Min_Max_Heap.py
@@ -0,0 +1,103 @@
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+
+    def push(self, value):
+        self.heap.append(value)
+        self._heapify_up(len(self.heap) - 1)
+
+    def pop(self):
+        if not self.heap:
+            return None
+
+        if len(self.heap) == 1:
+            return self.heap.pop()
+
+        root = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._heapify_down(0)
+        return root
+
+    def _heapify_up(self, index):
+        parent_index = (index - 1) // 2
+        while index > 0 and self.heap[index] < self.heap[parent_index]:
+            self.heap[index], self.heap[parent_index] = self.heap[parent_index], self.heap[index]
+            index = parent_index
+            parent_index = (index - 1) // 2
+
+    def _heapify_down(self, index):
+        left_child = 2 * index + 1
+        right_child = 2 * index + 2
+        smallest = index
+
+        if left_child < len(self.heap) and self.heap[left_child] < self.heap[smallest]:
+            smallest = left_child
+        if right_child < len(self.heap) and self.heap[right_child] < self.heap[smallest]:
+            smallest = right_child
+
+        if smallest != index:
+            self.heap[index], self.heap[smallest] = self.heap[smallest], self.heap[index]
+            self._heapify_down(smallest)
+
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+
+    def push(self, value):
+        self.heap.append(value)
+        self._heapify_up(len(self.heap) - 1)
+
+    def pop(self):
+        if not self.heap:
+            return None
+
+        if len(self.heap) == 1:
+            return self.heap.pop()
+
+        root = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self._heapify_down(0)
+        return root
+
+    def _heapify_up(self, index):
+        parent_index = (index - 1) // 2
+        while index > 0 and self.heap[index] > self.heap[parent_index]:
+            self.heap[index], self.heap[parent_index] = self.heap[parent_index], self.heap[index]
+            index = parent_index
+            parent_index = (index - 1) // 2
+
+    def _heapify_down(self, index):
+        left_child = 2 * index + 1
+        right_child = 2 * index + 2
+        largest = index
+
+        if left_child < len(self.heap) and self.heap[left_child] > self.heap[largest]:
+            largest = left_child
+        if right_child < len(self.heap) and self.heap[right_child] > self.heap[largest]:
+            largest = right_child
+
+        if largest != index:
+            self.heap[index], self.heap[largest] = self.heap[largest], self.heap[index]
+            self._heapify_down(largest)
+
+# Example usage for MinHeap
+min_heap = MinHeap()
+min_heap.push(5)
+min_heap.push(10)
+min_heap.push(3)
+min_heap.push(8)
+print("Min Heap elements:", min_heap.heap)
+
+print("Min Heap pop:", min_heap.pop())
+print("Min Heap elements after pop:", min_heap.heap)
+
+# Example usage for MaxHeap
+max_heap = MaxHeap()
+max_heap.push(5)
+max_heap.push(10)
+max_heap.push(3)
+max_heap.push(8)
+print("\nMax Heap elements:", max_heap.heap)
+
+print("Max Heap pop:", max_heap.pop())
+print("Max Heap elements after pop:", max_heap.heap)
